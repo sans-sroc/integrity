2a2cd90f3b0328443f7e04268f8b5e13b07e361c
ryananicholson <ryananicholson@gmail.com>
2021-03-15 10:44:15 -0400

    Close #3

--- integrity.go
+++ integrity.go
@@ -59,13 +59,92 @@ func createVerFile(verFile string) {
 
 // Add data for hashed file to VERSION file
 func appendVerFile(verFile string, fileName string, sha256String string) {
-	f, err := os.OpenFile(verFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	// Main VERSION file
+	f, err := os.OpenFile(verFile+".txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	check(err, "Cannot open file")
 	defer f.Close()
 	_, err = f.WriteString(fileName + ": " + sha256String + "\n")
 	check(err, "Cannot write to file")
 	err = f.Sync()
 	check(err, "Cannot write to file")
+
+	// Part VERSION file
+	if !strings.Contains(fileName, "get_first") {
+		f, err := os.OpenFile(verFile+"-part.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		check(err, "Cannot open file")
+		defer f.Close()
+		_, err = f.WriteString(fileName + ": " + sha256String + "\n")
+		check(err, "Cannot write to file")
+		err = f.Sync()
+		check(err, "Cannot write to file")
+	}
+}
+
+func validateFiles(directory string, version string, parts bool) {
+	var failed = false
+	verFile := ""
+	if parts {
+		verFile = directory + "/VERSION-" + version + "-part.txt"
+	} else {
+		verFile = directory + "/VERSION-" + version + ".txt"
+	}
+	_, err := os.Stat(verFile)
+	check(err, "Cannot find VERSION file")
+	verBytes, err := ioutil.ReadFile(verFile)
+	check(err, "Cannot read VERSION file")
+	vfString := string(verBytes)
+	cfileNames := make(map[string]string)
+	ofileNames := make(map[string]string)
+	re := regexp.MustCompile("(?m)^(^[^#][^:]+):(.*)$")
+	allFiles := re.FindAllStringSubmatch(vfString, -1)
+	for i := 0; i < len(allFiles); i++ {
+		ofileNames[allFiles[i][1]] = allFiles[i][2]
+	}
+	err = filepath.Walk(directory,
+		func(path string, info os.FileInfo, err error) error {
+			pathCheck, err2 := os.Stat(path)
+			check(err2, "Cannot find file")
+			if !pathCheck.IsDir() {
+				hash, err2 := hashFileSha256(path)
+				check(err2, "Cannot hash file")
+				fileName, err2 := filepath.Rel(directory, path)
+				check(err2, "Cannot find file")
+				if (fileName != "VERSION-"+version+".txt") && (fileName != "VERSION-"+version+"-part.txt") {
+					cfileNames[fileName] = hash
+				}
+				if parts {
+					if (!strings.Contains(vfString, fileName+": "+hash)) && (fileName != "VERSION-"+version+".txt") && (fileName != "VERSION-"+version+"-part.txt") && (!strings.Contains(fileName, "get_first")) {
+						fmt.Println("[!] Validation failed! File has been added!")
+						fmt.Println("    File: " + fileName)
+						fmt.Println("    Hash: " + hash)
+						failed = true
+					}
+				} else {
+					if (!strings.Contains(vfString, fileName+": "+hash)) && (fileName != "VERSION-"+version+".txt") && (fileName != "VERSION-"+version+"-part.txt") {
+						fmt.Println("[!] Validation failed! File has been added!")
+						fmt.Println("    File: " + fileName)
+						fmt.Println("    Hash: " + hash)
+						failed = true
+					}
+				}
+			}
+			return nil
+		})
+	for name, hash := range ofileNames {
+		if _, ok := cfileNames[name]; !ok {
+			fmt.Println("[!] Validation failed! Original file missing!")
+			fmt.Println("    File: " + name)
+			fmt.Println("    Hash: " + hash)
+			failed = true
+		}
+	}
+	check(err, "Validation failed")
+	fmt.Println("[+] Validation process complete!")
+	if failed {
+		fmt.Println("[!] Result: FAIL!")
+	} else {
+		fmt.Println("[+] Result: SUCCESS!")
+	}
 }
 
 // Main function
@@ -74,6 +153,7 @@ func main() {
 	validatePtr := flag.Bool("v", false, "Validate existing VERSION file")
 	verPtr := flag.String("c", "UNDEFINED", "Courseware Version Indentifier")
 	jsonPtr := flag.Bool("j", false, "Output data in JSON format to stdout")
+	partsPtr := flag.Bool("p", false, "Verify the VERSION-part.txt file")
 	flag.Parse()
 	jsonVal := *jsonPtr
 	jsonOutput := ""
@@ -89,20 +169,17 @@ func main() {
 	if !validateVal {
 		if !jsonVal {
 			fmt.Println("[+] Working directory:", *dirPtr)
-		}
-		_, err := os.Stat(*dirPtr + "/VERSION-" + *verPtr)
-		if err == nil {
-			fmt.Println("[!] VERSION file already exists! Overwriting!")
-			err = os.Remove(*dirPtr + "/VERSION-" + *verPtr)
-			check(err, "Cannot delete VERSION file")
-		}
-
-		if !jsonVal {
-			createVerFile(*dirPtr + "/VERSION-" + *verPtr)
+			_, err := os.Stat(*dirPtr + "/VERSION-" + *verPtr + ".txt")
+			if err == nil {
+				fmt.Println("[!] VERSION file already exists! Overwriting!")
+				err = os.Remove(*dirPtr + "/VERSION-" + *verPtr + ".txt")
+				check(err, "Cannot delete VERSION file")
+			}
+			createVerFile(*dirPtr + "/VERSION-" + *verPtr + ".txt")
 		} else {
 			jsonOutput = "{\n\t\"files\": [\n"
 		}
-		err = filepath.Walk(*dirPtr,
+		err := filepath.Walk(*dirPtr,
 			func(path string, info os.FileInfo, err error) error {
 				pathCheck, err2 := os.Stat(path)
 				check(err2, "Cannot process file")
@@ -114,17 +191,16 @@ func main() {
 					check(err2, "Cannot hash file")
 					fileName, err2 := filepath.Rel(*dirPtr, path)
 					check(err2, "Cannot determine file path")
-					if fileName != "VERSION-"+*verPtr && !jsonVal {
+					if (!strings.Contains(fileName, "VERSION-"+*verPtr)) && (!jsonVal) {
 						appendVerFile(*dirPtr+"/VERSION-"+*verPtr, fileName, hash)
 					}
-					if fileName != "VERSION-"+*verPtr && jsonVal {
+					if (!strings.Contains(fileName, "VERSION-"+*verPtr)) && (jsonVal) {
 						if !jsonFirst {
 							jsonOutput += ",\n"
 						}
 						jsonOutput = jsonOutput + "\t\t{\n\t\t\t\"fileName\": \"" + fileName + "\",\n\t\t\t\"hash\": \"" + hash + "\"\n\t\t}"
 						jsonFirst = false
 					}
-
 				}
 				return nil
 			})
@@ -134,56 +210,7 @@ func main() {
 			fmt.Println(jsonOutput)
 		}
 	} else {
-		// Validate existing VERSION file
-		var failed = false
-		verFile := *dirPtr + "/VERSION-" + *verPtr
-		_, err := os.Stat(verFile)
-		check(err, "Cannot find VERSION file")
-		verBytes, err := ioutil.ReadFile(verFile)
-		check(err, "Cannot read VERSION file")
-		vfString := string(verBytes)
-		cfileNames := make(map[string]string)
-		ofileNames := make(map[string]string)
-		re := regexp.MustCompile("(?m)^(^[^#][^:]+):(.*)$")
-		allFiles := re.FindAllStringSubmatch(vfString, -1)
-		for i := 0; i < len(allFiles); i++ {
-			ofileNames[allFiles[i][1]] = allFiles[i][2]
-		}
-		err = filepath.Walk(*dirPtr,
-			func(path string, info os.FileInfo, err error) error {
-				pathCheck, err2 := os.Stat(path)
-				check(err2, "Cannot find file")
-				if !pathCheck.IsDir() {
-					hash, err2 := hashFileSha256(path)
-					check(err2, "Cannot hash file")
-					fileName, err2 := filepath.Rel(*dirPtr, path)
-					check(err2, "Cannot find file")
-					if fileName != "VERSION-"+*verPtr {
-						cfileNames[fileName] = hash
-					}
-					if (!strings.Contains(vfString, fileName+": "+hash)) && (fileName != "VERSION-"+*verPtr) {
-						fmt.Println("[!] Validation failed! File has been added!")
-						fmt.Println("    File: " + fileName)
-						fmt.Println("    Hash: " + hash)
-						failed = true
-					}
-				}
-				return nil
-			})
-		for name, hash := range ofileNames {
-			if _, ok := cfileNames[name]; !ok {
-				fmt.Println("[!] Validation failed! Original file missing!")
-				fmt.Println("    File: " + name)
-				fmt.Println("    Hash: " + hash)
-				failed = true
-			}
-		}
-		check(err, "Validation failed")
-		fmt.Println("[+] Validation process complete!")
-		if failed {
-			fmt.Println("[!] Result: FAIL!")
-		} else {
-			fmt.Println("[+] Result: SUCCESS!")
-		}
+		// Validate existing VERSION file(s)
+		validateFiles(*dirPtr, *verPtr, *partsPtr)
 	}
 }
