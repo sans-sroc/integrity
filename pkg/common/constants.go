c1fe4c4d75086f325444b1802d9516f645d215d8
Erik Kristensen <erik@erikkristensen.com>
2021-05-17 10:46:33 -0600

    remove ? off of regex to fix matching on uppercase only, resolves #43

--- pkg/common/constants.go
+++ pkg/common/constants.go
@@ -1,6 +1,6 @@
 package common
 
-const NameFormat = "[0-9]{3}.[0-9]{2}.[0-9][A-Z]?"
+const NameFormat = "[0-9]{3}.[0-9]{2}.[0-9][A-Z]"
 
 const Filename = "sans-integrity.yml"
 const FilenameSigned = "sans-integrity.yml.gpg"
